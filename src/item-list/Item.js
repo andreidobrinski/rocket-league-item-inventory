ff8faac21766272e3dfadbdc22ba8b7a7076f6fb
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-06-18 17:39:05 -0400

    delete Item

--- src/item-list/Item.js
+++ src/item-list/Item.js
@@ -1,15 +1,49 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 
-const Item = ({ item }) => (
-  <Wrap>
-    <img src={item.imageLink} alt={item.name} />
-    <p>{item.name}</p>
-    <p>{item.rarity}</p>
-    {item.certification && <p>{item.certification}</p>}
-  </Wrap>
-);
+import { DestructiveButton, MutedButton } from '../app/styled';
+import { FirebaseContext } from '../auth/FirebaseContext';
+import { UserContext } from '../auth/UserContext';
+
+const Item = ({ item }) => {
+  const { firebase } = useContext(FirebaseContext);
+  const {
+    currentUser: { uid },
+  } = useContext(UserContext);
+  const [deleting, setDeleting] = useState(false);
+
+  if (deleting)
+    return (
+      <Wrap as="div">
+        Delete this item?
+        <DestructiveButton
+          onClick={() => {
+            firebase
+              .firestore()
+              .collection('users')
+              .doc(uid)
+              .collection('items')
+              .doc(item.id)
+              .delete();
+          }}
+          style={{ marginBottom: '8px' }}
+        >
+          Delete
+        </DestructiveButton>
+        <MutedButton onClick={() => setDeleting(false)}>Cancel</MutedButton>
+      </Wrap>
+    );
+
+  return (
+    <Wrap onClick={() => setDeleting(true)}>
+      <img src={item.imageLink} alt={item.name} />
+      <p>{item.name}</p>
+      <p>{item.rarity}</p>
+      {item.certification && <p>{item.certification}</p>}
+    </Wrap>
+  );
+};
 
 Item.propTypes = {
   item: PropTypes.shape({
@@ -23,11 +57,12 @@ Item.propTypes = {
   }).isRequired,
 };
 
-const Wrap = styled.div`
+const Wrap = styled.button.attrs(() => ({ type: 'button' }))`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  border: none;
   max-width: 250px;
   background: black;
   padding: 16px;
