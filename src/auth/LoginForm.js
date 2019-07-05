c8a5c746bc6066da3f102715e4344e90e6364292
Andrei Dobrinski <andreiidobrinski@gmail.com>
2019-07-05 11:19:33 -0400

    animate LoginForm

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 import { AppWrap, Form, Button } from '../app/styled';
 import { FirebaseContext } from './FirebaseContext';
@@ -28,32 +29,34 @@ const LoginForm = () => {
 
   return (
     <AppWrap>
-      <h2 style={{ textAlign: 'center' }}>Rocket League Item Inventory</h2>
-      <Wrap>
-        <Form onSubmit={handleSubmit}>
-          <label htmlFor="email">
-            Email
-            <input
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              type="text"
-              id="email"
-            />
-          </label>
-          <label htmlFor="password">
-            Password
-            <input
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              type="password"
-              id="password"
-            />
-          </label>
-          <Button type="submit" style={{ marginTop: '16px' }}>
-            Login
-          </Button>
-        </Form>
-      </Wrap>
+      <Fade bottom>
+        <h2 style={{ textAlign: 'center' }}>Rocket League Item Inventory</h2>
+        <Wrap>
+          <Form onSubmit={handleSubmit}>
+            <label htmlFor="email">
+              Email
+              <input
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                type="text"
+                id="email"
+              />
+            </label>
+            <label htmlFor="password">
+              Password
+              <input
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password"
+                id="password"
+              />
+            </label>
+            <Button type="submit" style={{ marginTop: '16px' }}>
+              Login
+            </Button>
+          </Form>
+        </Wrap>
+      </Fade>
     </AppWrap>
   );
 };
